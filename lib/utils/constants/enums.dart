f4745aa5d06b212294eb39263e077cd79a7aa527
nimendra <nimendraamiththa1@gmail.com>
2024-07-29 21:30:13 +0530

    there are a lot of file. I typed some files only. complete the other files as well. some dependancies are not working well in the device_utility.dart file. try to fix it as well.

---
+++ lib/utils/constants/enums.dart
@@ -0,0 +1,8 @@
+// List of Enums
+// They cannot be created inside a class
+
+enum TextSizes {small, medium, large}
+
+enum OrderStatus {processing, shipped, delivered}
+
+enum PayementMethods {paypal, googlePay, applePay, visa, masterCard, creditCard, paystack, razorPay, paytm}
---
+++ lib/utils/constants/image_strings.dart
@@ -0,0 +1,12 @@
+class TImages {
+  // -- App Logos
+  static const String darkAppLogo =
+      'assets/logos/t-store-spalsh-logo-black.png';
+
+  static const String lightAppLogo =
+      'assets/logos/t-store-spalsh-logo-black.png';
+
+  // -- Social Logos
+  static const String google = "assets/logos/google-icon.png";
+  static const String facebook = "assets/logos/facebook-icon.png";
+}
